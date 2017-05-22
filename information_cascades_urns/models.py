9dbcba51a54586a6f75c15089aae05af21d2426e
gavstrik <robin.engelhardt@gmail.com>
2017-05-22 15:35:11 +0200

    Fixed some errors

--- information_cascades_urns/models.py
+++ information_cascades_urns/models.py
@@ -26,43 +26,14 @@ class Subsession(BaseSubsession):
     def before_session_starts(self):
         self.storyline = random.choice(Constants.urns)
         for p in self.get_players():
-            print('#####', p.id_in_subsession)
-        # PS: see app cascades_bystander for an other solution with treatments creating one big group
-        # the method below creates as many groups as there are participants, one participant in each group
-        # by transposing the player matrix so that groups are ordered in columns
-        # if self.round_number == 1:
-        #     matrix = [list(i) for i in zip(*self.get_group_matrix())]
-        #     print(matrix)
-        #     self.set_group_matrix(matrix)
-        #     for g in self.get_groups():                         # different storylines for different groups
-        #         p1 = g.get_player_by_id(1)
-        #         p1.participant.vars['storyline'] = random.choice(Constants.urns) # save in participant vars
-        #         print(p1.participant.vars)
-        # else:
-        #     self.group_like_round(1)
-        #
-        # # retrieve storyline for each group from participant vars and find a ball for each player/group
-        # for g in self.get_groups():
-        #     p1 = g.get_player_by_id(1)
-        #     g.storyline = p1.participant.vars['storyline']
-        #     if p1.participant.vars['storyline'] == 'A':
-        #         p1.current_ball = random.choice(Constants.urn_A)
-        #         if self.round_number == 1 or self.round_number == 2:    # generate first two player choices
-        #             if p1.current_ball == 'red':
-        #                 p1.choice_of_urn = 'A'
-        #             else:
-        #                 p1.choice_of_urn = 'B'
-        #     else:
-        #         p1.current_ball = random.choice(Constants.urn_B)
-        #         if self.round_number == 1 or self.round_number == 2:    # generate first two player choices
-        #             if p1.current_ball == 'red':
-        #                 p1.choice_of_urn = 'A'
-        #             else:
-        #                 p1.choice_of_urn = 'B'
+            if self.storyline == 'A':
+                p.current_ball = random.choice(Constants.urn_A)
+            else:
+                p.current_ball = random.choice(Constants.urn_B)
+            print('#####', p.id_in_subsession, self.storyline, p.current_ball)
 
 class Group(BaseGroup):
     ...
-    # storyline = models.CharField()
 
 class Player(BasePlayer):
     current_ball = models.CharField()                   # the color of the ball a player will draw
--- information_cascades_urns/views.py
+++ information_cascades_urns/views.py
@@ -2,14 +2,7 @@ from . import models
 from ._builtin import Page, WaitPage
 from .models import Constants
 
-class Introduction(Page):
-    def is_displayed(self):
-        return self.subsession.round_number == 1
-
 class Choose(Page):
-    # def is_displayed(self):
-    #     return self.round_number > 2    # only shown after round 2
-
     form_model = models.Player          # setting a form model for current player
     form_fields = ['choice_of_urn']     # setting a form field to fill out
 
@@ -28,36 +21,20 @@ class ResultsWaitPage(WaitPage):
     pass
 
 class Results(Page):
-    # def is_displayed(self):
-    #     self.player.set_payoffs()
-    #     return self.round_number > 2 and self.round_number < Constants.num_rounds
+    def is_displayed(self):
+        self.player.set_payoffs()
+        return True
 
     def vars_for_template(self):
         previous_players = [p for p
                             in self.subsession.get_players()
                             if p.choice_of_urn]
         return {
-            #'total_performance': sum([p.payoff for p in self.player.in_all_rounds()]),
             'total_performance': self.player.payoff + Constants.endowment,
             'player_in_all_rounds': previous_players,
         }
 
-class FinalResults(Page):
-    def is_displayed(self):
-        self.player.set_payoffs()
-        return self.round_number == Constants.num_rounds
-
-    def vars_for_template(self):
-        return {
-            'total_performance': sum(
-                [p.payoff for p in self.player.in_all_rounds()]),
-            'player_in_all_rounds': self.player.in_all_rounds(),
-        }
-
-
 page_sequence = [
-    #Introduction,
     Choose,
     Results,
-    #FinalResults
 ]
